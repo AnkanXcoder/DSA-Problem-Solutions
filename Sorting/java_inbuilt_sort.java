48139497ffc9138b69112d2af8373cef488e6f1c
AnkanXcoder <ankansen703@gmail.com>
2026-03-23 19:24:28 +0530

    java inbuilt sort

--- Sorting/java_inbuilt_sort.java
+++ Sorting/java_inbuilt_sort.java
@@ -1,15 +1,19 @@
 import java.util.Arrays;
+import java.util.Collections;
 public class Main {
-    public static void printArr(int arr[]) {
+    public static void printArr(Integer arr[]) {
         for(int i=0;i<arr.length;i++) {
             System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
 	public static void main(String[] args) {
-	    int arr[] = {14,10,2,5,6,8,9,21,4};
-	    Arrays.sort(arr);
-	    printArr(arr);
+	    Integer arr[] = {5,4,6,3,2,8};
+	   // Arrays.sort(arr);
+	   // Arrays.sort(arr,0,5);
+	   //Arrays.sort(arr,Collections.reverseOrder());
+	   Arrays.sort(arr,0,3,Collections.reverseOrder());
+	   printArr(arr);
 	}
 }
 
